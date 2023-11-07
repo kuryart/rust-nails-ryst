1477fcb41b6a3e0b0d0d3a661ae0b8e01e8c99a1
kuryart <kuryart1@gmail.com>
2023-11-07 03:03:52 +0000

    create asset-pipeline crate

---
+++ crates/asset-pipeline/build.rs
@@ -0,0 +1,16 @@
+// fn main() -> Result<()>  {
+
+//     cornucopia()?;
+
+//     let mut ructe = Ructe::from_env().unwrap();
+//     let mut statics = ructe.statics().unwrap();
+//     statics.add_files("dist").unwrap();
+//     statics.add_files("images").unwrap();
+//     ructe.compile_templates("templates").unwrap();
+
+//     Ok(())
+// }
+
+fn main() {
+    print!("hello world")
+}
